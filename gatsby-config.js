f8e9da7483a09b24764b3ad420cf84e770130f02
Carlos Quintero Grande <cquinterogrande@gmail.com>
2021-05-14 13:07:05 -0600

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby con Autenticacion de  Firebase `,
+    description: `Projecto de Autenticacion de Firebase con Gatsby`,
+    author: `@caqg`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
